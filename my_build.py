abb9f9fbd454e52015ab27ae668309659a533f1c
Худяков Юрий <catchitoo@mail.ru>
2020-06-21 04:16:08 +0500

    Add notes to q22-24

--- my_build.py
+++ my_build.py
@@ -2,6 +2,8 @@
 
 import subprocess as su
 
+n = int(input())
+
 conspect="""
 
 
@@ -47,25 +49,26 @@ su.run(['mkdir', pdir])
 
 for d in range(0, 5):
     for j in range(ra[d][0], ra[d][1]+1):
-        s = str(j)
-        if j < 10:
-            s = '0'+s
-        con = conspect % (s, na[d], 'question' + s)
-        with open('%s/conspect.tex' % path, 'w') as o:
-            o.write(con)
-        su.run(['./build.py'])
-        rc = su.run(['diff',path2, '%s/question%s.pdf' % (pdir, s)]).returncode
-        if rc == 2:
-            su.run(['cp', path2, '%s/.question%s_orig.pdf' % (pdir, s)])
-            su.run(['mv', path2, '%s/question%s.pdf' % (pdir, s)])
-        elif rc == 1:
-            rc2 = su.run(['diff', '%s/question%s.pdf' % (pdir, s), '%s/.question%s_orig.pdf' % (pdir, s)]).returncode
-            if rc2 == 0:
+        if n == -1 or n == j:
+            s = str(j)
+            if j < 10:
+                s = '0'+s
+            con = conspect % (s, na[d], 'question' + s)
+            with open('%s/conspect.tex' % path, 'w') as o:
+                o.write(con)
+            su.run(['./build.py'])
+            rc = su.run(['diff',path2, '%s/question%s.pdf' % (pdir, s)]).returncode
+            if rc == 2:
                 su.run(['cp', path2, '%s/.question%s_orig.pdf' % (pdir, s)])
                 su.run(['mv', path2, '%s/question%s.pdf' % (pdir, s)])
-                print('update %s' % (s))
+            elif rc == 1:
+                rc2 = su.run(['diff', '%s/question%s.pdf' % (pdir, s), '%s/.question%s_orig.pdf' % (pdir, s)]).returncode
+                if rc2 == 0:
+                    su.run(['cp', path2, '%s/.question%s_orig.pdf' % (pdir, s)])
+                    su.run(['mv', path2, '%s/question%s.pdf' % (pdir, s)])
+                    print('update %s' % (s))
+                else:
+                    print('cannot change %s: file exists and changed' % (s))
             else:
                 print('cannot change %s: file exists and changed' % (s))
-        else:
-            print('cannot change %s: file exists and changed' % (s))
-        
+            
